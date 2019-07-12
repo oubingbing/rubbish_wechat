ce6f7b4f70d806239e3b0abb4f84aa09d7c1eb52
ouzhibing <ouzhibing@galaxymx.com>
2019-07-12 18:30:09 +0800

    测试

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config={
   dev:{//开发环境
     domain:"http://127.0.0.1:8000/api/wechat",
-    qiniuDomain:"https://article.qiuhuiyi.cn",
+    qiniuDomain:"https://image.qiuhuiyi.cn",
   },
   prod:{//生产环境
     domain: "https://heping.qiuhuiyi.cn/api/wechat",
-    qiniuDomain: "https://article.qiuhuiyi.cn"
+    qiniuDomain: "https://image.qiuhuiyi.cn"
   }
 }
 
